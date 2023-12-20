631f27e8d307f7b49af074ba7243b4713f6a32ec
Sahand Ghavidel <sahand.gha@gmail.com>
2023-12-20 07:46:49 +0700

    add recent article section to the post page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,5 +8,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
+  plugins: [
+    require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
+  ],
 };
